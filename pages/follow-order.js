5f3c628470e5d25bfca26d0d3abf72ae7395b01c
moicben <benjamain.georges@gmail.com>
2025-04-26 04:40:27 +0700

    v1 fixs

--- pages/follow-order.js
+++ pages/follow-order.js
@@ -30,8 +30,6 @@ const TrackOrder = ({ shop, data, categories, reviews }) => {
     return (
         <div className="container">
             <Head name={shop.name} domain={shop.domain}
-                favicon={brand.favicon} graph={brand.graph}
-                colorPrimary={brand.colorPrimary} colorSecondary={brand.colorSecondary} colorBlack={brand.colorBlack} colorGrey={brand.colorGrey} bgMain={brand.bgMain} bgLight={brand.bgLight} bgDark={brand.bgDark} radiusBig={brand.radiusBig} radiusMedium={brand.radiusMedium} font={brand.font} 
                 title={`${data.followPageLabel} - ${shop.name}`}
             />
             
