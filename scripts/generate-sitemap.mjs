57c56c1049772c034234416332f20a2aaadcaaab
moicben <benjamain.georges@gmail.com>
2025-04-27 10:54:50 +0700

    Update Sitemap Gen

--- scripts/generate-sitemap.mjs
+++ scripts/generate-sitemap.mjs
@@ -24,12 +24,10 @@ const staticPages = [
 
 const generateSitemap = async () => {
   // Récupération des données dynamiques depuis Supabase
-  const categories = await fetchData('categories', { match: { shop_id: process.env.SHOP_ID } });
   const products = await fetchData('products', { match: { shop_id: process.env.SHOP_ID } });
   const contents = await fetchData('contents', { match: { shop_id: process.env.SHOP_ID } });
-  const posts = await fetchData('posts', { match: { shop_id: process.env.SHOP_ID } });
 
-  if (!categories || !products || !contents) {
+  if (!products || !contents) {
     throw new Error('Erreur lors de la récupération des données depuis Supabase.');
   }
 
@@ -43,42 +41,20 @@ const generateSitemap = async () => {
     </url>
   `);
 
-  // Génération des URLs pour les catégories
-  const categoryUrls = categories.map((category) => `
+  // Génération des URLs pour les produits
+  const productUrls = products.map((product) =>  `
     <url>
-      <loc>${siteUrl}/${category.slug}</loc>
+      <loc>${siteUrl}/${product.slug}</loc>
       <lastmod>${new Date().toISOString()}</lastmod>
       <changefreq>weekly</changefreq>
-      <priority>0.8</priority>
+      <priority>0.7</priority>
     </url>
   `);
 
-  // Génération des URLs pour les produits
-  const productUrls = products.map((product) => {
-    const category = categories.find((cat) => cat.id === product.category_id);
-    if (!category) return ''; // Ignorer les produits sans catégorie correspondante
-    return `
-      <url>
-        <loc>${siteUrl}/${category.slug}/${product.slug}</loc>
-        <lastmod>${new Date().toISOString()}</lastmod>
-        <changefreq>weekly</changefreq>
-        <priority>0.7</priority>
-      </url>
-    `;
-  }).filter(Boolean); // Supprimer les entrées vides
-
-  // Génération des URLs pour les articles de blog
-  const blogUrls = posts.map((post) => `
-    <url>
-      <loc>${siteUrl}/blog/${post.slug}</loc>
-      <lastmod>${new Date().toISOString()}</lastmod>
-      <changefreq>monthly</changefreq> 
-      <priority>0.6</priority>
-    </url>
-  `);
+  
 
   // Combiner toutes les URLs
-  const urls = [...staticUrls, ...categoryUrls, ...productUrls, ...blogUrls];
+  const urls = [...staticUrls,  ...productUrls];
 
   // Retourner le contenu complet du sitemap
   return `<?xml version="1.0" encoding="UTF-8"?>
